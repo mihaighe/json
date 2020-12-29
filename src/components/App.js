7c8163a111b984b80a1ac528c04e9db76c1dd076
MihaiGhe <mihai.md16@gmail.com>
2020-12-29 13:13:15 +0200

    Screenshots

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import PostList from './PostList';
+import React from "react";
+import PostList from "./PostList";
+import logo from "../json.png"
 
 const App = () => {
   return (
-    <div className="ui container">
-      <PostList />
-    </div>
+      <div className="ui container">
+            <img alt="logo" src={logo} style={{borderBottom: "7px solid black"}}></img>
+            <PostList />
+      </div>
   );
 };
 
